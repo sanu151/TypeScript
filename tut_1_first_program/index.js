2a961b99e993f589bd4d2e34abc6af3e02efaed0
Supriyo Das <das.supriyo07@gmail.com>
2024-10-03 06:48:56 +0530

    First Program

---
+++ tut_1_first_program/index.js
@@ -0,0 +1,6 @@
+var userName = "Supriyo Das";
+console.log(userName);
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(23, 30);
---
+++ tut_1_first_program/index.ts
@@ -0,0 +1,7 @@
+let userName = "Supriyo Das";
+console.log(userName);
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+addNumbers(23, 30);
