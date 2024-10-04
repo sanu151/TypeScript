2fe1ab068817d86286125550a3eb887ac19f9048
Supriyo Das <das.supriyo07@gmail.com>
2024-10-04 06:28:23 +0530

    Tuple data type

---
+++ tut_3_user_defined_data_type/tuple-datatype.js
@@ -0,0 +1,6 @@
+// Mixed data type : tuple
+var users;
+users = [101, "Supriyo"];
+console.log(users);
+users.push(102, "Rishika");
+console.log(users);
---
+++ tut_3_user_defined_data_type/tuple-datatype.ts
@@ -0,0 +1,10 @@
+// Mixed data type : tuple
+
+let users: [number, string];
+
+users = [101, "Supriyo"];
+console.log(users);
+
+users.push(102, "Rishika");
+
+console.log(users);
