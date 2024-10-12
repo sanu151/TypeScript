d64c808c5795078625a63942df8330c23d3b1d5d
Supriyo Das <das.supriyo07@gmail.com>
2024-10-12 17:39:38 +0530

    Function Signature

--- tut_8_functionSignature/index.js
+++ tut_8_functionSignature/index.js
@@ -1,24 +1,27 @@
 // function - void: return nothing
-var display = function () {
+var display;
+display = function () {
     console.log("Hello World!");
 };
 display();
-// function with paramater : number
-var addNumbers = function (num1, num2) {
+// function with paramater : number, returns : number
+var addNumbers;
+addNumbers = function (num1, num2) {
     var result = num1 + num2;
     return result;
 };
 console.log(addNumbers(22, 45));
-// function with parameter : object
-var person = function (name, age, gender) {
+// function with parameter : object, returns: void
+var person;
+person = function (name, age, gender) {
     var data = {
         name: name,
         age: age,
         gender: gender,
     };
-    return data;
+    console.log("Name : ".concat(data.name));
+    console.log("Age : ".concat(data.age));
+    console.log("Gender : ".concat(data.gender));
 };
 var person1 = person("Supriyo Das", 37, "Male");
-console.log("Name : ".concat(person1.name));
-console.log("Age : ".concat(person1.age));
-console.log("Gender : ".concat(person1.gender));
+person1;
--- tut_8_functionSignature/index.ts
+++ tut_8_functionSignature/index.ts
@@ -1,26 +1,29 @@
 // function - void: return nothing
-const display = (): void => {
+let display: () => void;
+display = () => {
   console.log("Hello World!");
 };
 display();
 
-// function with paramater : number
-const addNumbers = (num1: number, num2: number): number => {
+// function with paramater : number, returns : number
+let addNumbers: (num1: number, num2: number) => number;
+addNumbers = (num1: number, num2: number) => {
   let result = num1 + num2;
   return result;
 };
 console.log(addNumbers(22, 45));
 
-// function with parameter : object
-const person = (name: string, age: number, gender: string) => {
+// function with parameter : object, returns: void
+let person: (name: string, age: number, gender: string) => void;
+person = (name: string, age: number, gender: string) => {
   let data: { name: string; age: number; gender: string } = {
     name: name,
     age: age,
     gender: gender,
   };
-  return data;
+  console.log(`Name : ${data.name}`);
+  console.log(`Age : ${data.age}`);
+  console.log(`Gender : ${data.gender}`);
 };
 const person1 = person("Supriyo Das", 37, "Male");
-console.log(`Name : ${person1.name}`);
-console.log(`Age : ${person1.age}`);
-console.log(`Gender : ${person1.gender}`);
+person1;
